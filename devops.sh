fb687b0629214252df4374924e4c758f460b3852
mahi <maheshwari1807@gmail.com>
2023-10-11 10:52:28 +0100

    commit stash

--- devops.sh
+++ devops.sh
@@ -1,3 +1,7 @@
+
+this is my staging testing 
+stash is used when the developer wants to switch to different task or branch.
+develpers temporily sane the their work wothout commit it
 hi
 
 this is my devops project
